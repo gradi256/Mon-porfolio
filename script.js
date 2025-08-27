8dac96ec8a9dbd6e4fdc7ee016f7f55f9b9845fc
Gradi Nawej <gradinawej8@gmail.com>
2025-08-27 16:51:42 +0100

    première version du site

---
+++ script.js
@@ -0,0 +1,9 @@
+// Exemple simple : petit effet sur le bouton
+document.querySelectorAll(".btn").forEach(btn => {
+  btn.addEventListener("mouseover", () => {
+    btn.style.transform = "scale(1.05)";
+  });
+  btn.addEventListener("mouseout", () => {
+    btn.style.transform = "scale(1)";
+  });
+});
